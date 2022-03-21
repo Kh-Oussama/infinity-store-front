f519789ce4ec4f02355cbe14fba38cbfc22fcd5d
aymensoudani <aymensoudani10@gmail.com>
2022-03-21 23:45:37 +0100

    Add Suspense loader & product loader

---
+++ src/components/EyeLoader/EyeLoader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const EyeLoader = ({active}) => {
+    return (
+        <div className={`eye-loader ${active}`}>
+            <div className="big-eye">
+                <div className="small-eye"></div>
+            </div>
+        </div>
+    );
+}
+
+export default EyeLoader;
