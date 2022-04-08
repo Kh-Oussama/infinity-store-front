26c68efcd47ed32495dc47d2c24cea4d5cb2dc27
aymensoudani <aymensoudani10@gmail.com>
2022-04-08 11:08:05 +0100

    Sign responsive

--- src/components/bottom-navigation/bottom-navigation.component.jsx
+++ src/components/bottom-navigation/bottom-navigation.component.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import {createStructuredSelector} from "reselect";
+import {toggleAuthComponent} from "../../redux/design-utilites/design-utilities.actions";
 
-const BottomNavigation = _ => {
+const BottomNavigation = ({toggleAuthComponent}) => {
     return (
         <div className="bottom-navigation">
-            <span className="fas fa-align-left"></span>
-            <span className="fas fa-search"></span>
             <span className="fas fa-home"></span>
+            <span className="fas fa-search"></span>
             <span className="fas fa-archive"></span>
-            <span className="fas fa-user"></span>
+            <span onClick={() => toggleAuthComponent("sign-in")} className="fas fa-user"></span>
         </div>
     );
 }
 
-export default BottomNavigation;
+const mapStateToProps = createStructuredSelector({
+});
+
+const mapDispatchToProps = dispatch => ({
+    //change the auth popup view state
+    toggleAuthComponent: current_component => dispatch(toggleAuthComponent(current_component)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BottomNavigation);
