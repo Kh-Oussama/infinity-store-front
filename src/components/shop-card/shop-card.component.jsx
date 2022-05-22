76ee76558309a2d1cece774c2578605cc384b006
aymensoudani <aymensoudani10@gmail.com>
2022-05-22 02:33:23 +0100

    Responsive

--- src/components/shop-card/shop-card.component.jsx
+++ src/components/shop-card/shop-card.component.jsx
@@ -4,36 +4,36 @@ import React from 'react';
 import ShoppingBagIcon from "../icons/shopingBag";
 import ShoppingCardPopup from "../shopping-card-popup/shopping-card-popup-component";
 import ContentLoader from "react-content-loader";
-import {createStructuredSelector} from "reselect";
-import {selectShopCard} from "./../../redux/design-utilites/design-utilities.selectors";
-import {toggleShopCard} from "./../../redux/design-utilites/design-utilities.actions";
+import { createStructuredSelector } from "reselect";
+import { selectShopCard } from "./../../redux/design-utilites/design-utilities.selectors";
+import { toggleShopCard } from "./../../redux/design-utilites/design-utilities.actions";
 import { connect } from 'react-redux';
-import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors";
+import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 
 
 //the left shopping card component
-const ShopCard = ({loading, shopCardDisplayed, toggleShopCard,cartItems, history, total}) => {
+const ShopCard = ({ loading, shopCardDisplayed, toggleShopCard, cartItems, history, total }) => {
 
     const toggleModal = () => {
         toggleShopCard(!shopCardDisplayed);
-        if (!shopCardDisplayed)  document.body.style.overflow = 'hidden';
-        else  document.body.style.overflow = 'unset';
+        if (!shopCardDisplayed) document.body.style.overflow = 'hidden';
+        else document.body.style.overflow = 'unset';
     };
 
     return (
         <>
-            <ShoppingCardPopup toggleModal={toggleModal} showModal={shopCardDisplayed}/>
-            <div className="shop-card" style={{backgroundColor: loading ?'transparent' : null}} onClick={() => toggleModal()}>
-                            <div className="items-count">
-                                {/*this is for the icon*/}
-                                <ShoppingBagIcon/>
+            <ShoppingCardPopup toggleModal={toggleModal} showModal={shopCardDisplayed} />
+            <div className="shop-card" style={{ backgroundColor: loading ? 'transparent' : null }} onClick={() => toggleModal()}>
+                <div className="items-count">
+                    {/*this is for the icon*/}
+                    <ShoppingBagIcon />
 
-                                <div className="count-number">{cartItems.length}</div>
-                                <div className={"items"}>Item</div>
-                            </div>
-                            <div className="total">
-                                {total} DA
-                            </div>
+                    <div className="count-number">{cartItems.length}</div>
+                    <div className={"items"}>Item</div>
+                </div>
+                <div className="total">
+                    {total} DA
+                </div>
             </div>
         </>
     )
@@ -43,7 +43,7 @@ const ShopCard = ({loading, shopCardDisplayed, toggleShopCard,cartItems, history
 const mapStateToProps = createStructuredSelector({
     shopCardDisplayed: selectShopCard,
     cartItems: selectCartItems,
-    total : selectCartTotal,
+    total: selectCartTotal,
 });
 
 
